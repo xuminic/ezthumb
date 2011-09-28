83b79abd3a75482d4cf011b6c4533598dea0ce79
Andy Xuming <xuming@users.sf.net>
2011-09-28 15:26:57 +1000

    Added version display of FFMPEGs

--- main.c
+++ main.c
@@ -108,6 +108,22 @@ int main(int argc, char **argv)
 			return 0;
 		case 2:
 			printf(version, EZTHUMB_VERSION);
+			printf("FFMPEG: libavcodec %d.%d.%d; ", 
+					LIBAVCODEC_VERSION_MAJOR, 
+					LIBAVCODEC_VERSION_MINOR,
+					LIBAVCODEC_VERSION_MICRO);
+			printf("libavformat %d.%d.%d; ", 
+					LIBAVFORMAT_VERSION_MAJOR,
+					LIBAVFORMAT_VERSION_MINOR,
+					LIBAVFORMAT_VERSION_MICRO);
+			printf("libavutil %d.%d.%d; ",
+					LIBAVUTIL_VERSION_MAJOR,
+					LIBAVUTIL_VERSION_MINOR,
+					LIBAVUTIL_VERSION_MICRO);
+			printf("libswscale %d.%d.%d\n",
+					LIBSWSCALE_VERSION_MAJOR,
+					LIBSWSCALE_VERSION_MINOR,
+					LIBSWSCALE_VERSION_MICRO);
 			return 0;
 		case 6:	/* nonkey */
 			sysoption.flags |= EZOP_P_FRAME;
